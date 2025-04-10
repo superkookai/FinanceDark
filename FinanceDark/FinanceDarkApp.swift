6f52c14a3ae99896a7acea7334f4d923979634de
superkookai <weerawut@live.com>
2025-04-10 09:07:21 +0700

    Initial Commit

---
+++ FinanceDark/FinanceDarkApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinanceDarkApp.swift
+//  FinanceDark
+//
+//  Created by Weerawut Chaiyasomboon on 10/04/2568.
+//
+
+import SwiftUI
+
+@main
+struct FinanceDarkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
